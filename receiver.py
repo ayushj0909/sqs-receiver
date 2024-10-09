0f2513c7a422d4528ab7d5617a8e3685d692a3c2
Ayush Jain <ayush@amnic.com>
2024-10-09 15:25:04 +0530

    feat: sqs-receiver

---
+++ receiver.py
@@ -0,0 +1,34 @@
+import boto3
+import os
+import time
+
+# Get AWS credentials and SQS queue URL from environment variables
+aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID']
+aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
+sqs_queue_url = os.environ['SQS_QUEUE_URL']
+
+# Create an SQS client
+sqs_client = boto3.client(
+    'sqs',
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key,
+)
+
+while True:
+    response = sqs_client.receive_message(
+        QueueUrl=sqs_queue_url,
+        MaxNumberOfMessages=10,
+        WaitTimeSeconds=10,
+    )
+
+    messages = response.get('Messages', [])
+    for message in messages:
+        print(f"Received message: {message['Body']}")
+
+        # Delete the message after processing
+        sqs_client.delete_message(
+            QueueUrl=sqs_queue_url,
+            ReceiptHandle=message['ReceiptHandle'],
+        )
+
+    time.sleep(5)
