a0abf8622a32e1d0a76cdeb9c329186f0f64fe58
Ayush Jain <ayush@amnic.com>
2024-10-09 20:42:32 +0530

    chore(sender): passing sleep time through env var

--- sender/sender.py
+++ sender/sender.py
@@ -9,7 +9,7 @@ logger = logging.getLogger()
 aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID']
 aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
 sqs_queue_url = os.environ['SQS_QUEUE_URL']
-
+sleep_time = int(os.environ['SLEEP_TIME'])
 region_name = 'ap-south-1'  # Change to your region
 
 sqs_client = boto3.client(
@@ -37,4 +37,4 @@ while True:
     except Exception as e:
         logger.error(f"An error occurred while sending message: {e}")
 
-    time.sleep(10)
+    time.sleep(sleep_time)
